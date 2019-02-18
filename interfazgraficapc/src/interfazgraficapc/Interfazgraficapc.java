83742ceb8e7482957d2971d09837c621913b1c34
alumno <alumno@aula1-15>
2019-02-18 18:40:15 +0100

    1

---
+++ interfazgraficapc/src/interfazgraficapc/Interfazgraficapc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfazgraficapc;
+
+/**
+ *
+ * @author alumno
+ */
+public class Interfazgraficapc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pantalla p = new Pantalla();
+        
+        
+        
+    }
+    
+}
